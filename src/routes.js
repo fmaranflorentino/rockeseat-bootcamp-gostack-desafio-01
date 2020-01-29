9083efc615c131c7acff9bc35703776066ad2959
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-01-29 21:28:56 -0200

    Revert "fix(routes): Removed un unsed variable"
    This reverts commit e3411bfacf6ef10b3862ae128a1efdd068ea0413.

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import projectsRoutes from './routes/projects';
 const routes = new Router();
 
+let requests = 0;
+
 routes.use((req, res, next) => {
   requests++;
   console.log(`Number of request send until now ${requests}`);
